a9d2841eb42f34d35884c19f9941496eebefac3d
Ömer Cengiz <omer@Omers-MacBook-Air.local>
2024-05-16 15:54:25 +0300

    added notes

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
-
+// Testing Process will be added
 describe('MainContent', () => {
   it('should render a button', () => {
     render(<MainContent />);
